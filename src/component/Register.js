09f36029cec0ca79083093fbe2b74b3c12846c87
goodweather37 <140213173+goodweather37@users.noreply.github.com>
2024-07-11 20:31:00 +0930

    updated styles

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,15 +1,19 @@
 export default function Register() {
     return (
-        <div className="wide">
+        <div className="container">
             <h3 className="heading">Register</h3>
             <form>
                 <div>
-                    <label className="formLabel">Email Address</label>
-                    <input className="formInput" type="email" placeholder="Enter email" />
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" name="email" placeholder="Enter email" required />
                 </div>
                 <div>
-                    <label className="formLabel">Password</label>
-                    <input className="formInput" type="password" placeholder="Password" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" placeholder="Password" required />
+                </div>
+                <div>
+                    <label htmlFor="repeat-password">Repeat Password</label>
+                    <input type="password" name="repeat-password" placeholder="Repeat Password" required />
                 </div>
                 <button className="submitBtn" type="submit">Submit</button>
             </form>
